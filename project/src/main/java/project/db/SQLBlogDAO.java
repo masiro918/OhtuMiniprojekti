a7956ddc4c5ffb754d27d92c82b96c95a204959a
Matias Siro <matias.siro@outlook.com>
2021-11-25 14:33:53 +0200

    lisätty metodeja luokkaan SQLBlogDAO.java

--- project/src/main/java/project/db/SQLBlogDAO.java
+++ project/src/main/java/project/db/SQLBlogDAO.java
@@ -1,5 +1,64 @@
 package project.db;
 
-public class SQLBlog {
+import project.domain.BlogRecommendation;
+import java.sql.*;
 
+/**
+ * Luokka käsittelee BlogRecommendation-olioden tallennuksen
+ * ja poiston tietokannassa.
+ */
+public class SQLBlogDAO {
+    private Connection connection = null;
+    private Statement statement = null;
+
+    /**
+     * Konstruktori.
+     */
+    public SQLBlogDAO() {
+
+    }
+
+    /**
+     * Tallentaa blogi-vinkin tietokantaan.
+     * @param blogRecommendation lisättävä olio
+     *  @throws Exception
+     */
+    public void add(BlogRecommendation blogRecommendation) throws Exception {
+        this.createConnection();
+
+        String sql = "";
+
+        statement.executeUpdate(sql);
+        this.closeConnection();
+    }
+
+    /**
+     * Poistaa blogi-vinkit tietokannasta.
+     * @param blogRecommendation poistettava olio
+     * @throws Exception
+     */
+    public void remove(BlogRecommendation blogRecommendation) throws Exception {
+
+    }
+
+    /**
+     * Luo uuden tietokanta-yhetyden.
+     * @throws Exception
+     */
+    private void createConnection() throws Exception {
+        this.connection = DriverManager.getConnection("jdbc:sqlite:database.db");
+        this.statement = connection.createStatement();
+    }
+
+    /**
+     * Suolkee tietokanta-yhteyden.
+     * @throws Exception
+     */
+    private void closeConnection() throws Exception {
+        this.statement.close();
+        this.connection.close();
+        
+        this.connection = null;
+        this.statement = null;
+    }
 }
