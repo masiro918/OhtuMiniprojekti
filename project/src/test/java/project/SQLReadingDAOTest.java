024b1e0970c645744e7897d6b5a2c7d76cc23b02
Matias Siro <matias.siro@outlook.com>
2021-11-30 19:32:54 +0200

    lisätty runko testeille

---
+++ project/src/test/java/project/SQLReadingDAOTest.java
@@ -0,0 +1,13 @@
+package project;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SQLReadingDAOTest {
+
+    @Before
+    public void setUp() {
+        
+    }
+}
