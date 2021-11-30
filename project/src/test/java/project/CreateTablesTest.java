50fecf57d55d2bcb115d53f7ff75be028b20d69a
Matias Siro <matias.siro@outlook.com>
2021-11-30 19:55:55 +0200

    lisätty koodin runkoa hieman lisää

--- project/src/test/java/project/CreateTablesTest.java
+++ project/src/test/java/project/CreateTablesTest.java
@@ -7,8 +7,35 @@ import static org.junit.Assert.*;
 
 public class SQLCreateTablesTest {
 
+    private Connection connection = null;
+    private Statement statement = null;
+
     @Before
     public void setUp() {
-        
+        TableCreator tc = new TableCreator();
     }
+
+    @Test
+    public void createUserTableTest() throws Exception {
+        /*
+        tc.createUser();
+
+        createConnection();
+        */
+    }
+
+    /*
+    private void createConnection() throws SQLException {
+        this.connection = DriverManager.getConnection("jdbc:sqlite:database.db");
+        this.statement = connection.createStatement();
+    }
+    
+    private void closeConnection() throws SQLException {
+        this.statement.close();
+        this.connection.close();
+        
+        this.connection = null;
+        this.statement = null;
+    }
+    */
 }
