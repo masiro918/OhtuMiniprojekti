d3283a0cd0494f366ef62d37d8283d3b2511adcd
Heli Eronen <herojonen@gmail.com>
2021-11-30 11:57:33 +0200

    lisätty javadoc

--- project/src/test/java/project/FakeUserDAO.java
+++ project/src/test/java/project/FakeUserDAO.java
@@ -1,32 +1,54 @@
-
 package project;
 
 import java.util.ArrayList;
 import project.db.UserDAO;
 import project.domain.UserInterface;
 
-public class FakeUserDAO implements UserDAO{
-    ArrayList<UserInterface> fakeUsers;
+/**
+ * A fake database for AuthenticationServiceTest-class.
+ */
+public class FakeUserDAO implements UserDAO {
+    private ArrayList<UserInterface> fakeUsers;
     
     public FakeUserDAO() {
         this.fakeUsers = new ArrayList<UserInterface>();
     }
     
+    /**
+     * Adds user to "database".
+     * 
+     * @param user user that is added
+     */
     @Override
     public void add(UserInterface user) {
         this.fakeUsers.add(user);
     }
-
+    
+    /**
+     * Gets all user's information from the "database".
+     * 
+     * @return true if user was found, false if not.
+     */
     @Override
     public ArrayList<UserInterface> fetchAllUsers() {
         return this.fakeUsers;
     }
-
+    
+    /**
+     * Adds user to "database".
+     * 
+     * @param user user that is added
+     */
     @Override
     public boolean login(UserInterface user) throws Exception {
         return true;
     }
-
+    
+    /**
+     * Removes user from "database".
+     * 
+     * @param user user that is removed
+     */
     @Override
     public void remove(UserInterface user) throws Exception {
         this.fakeUsers.remove(user);
