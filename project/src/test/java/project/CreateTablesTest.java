924b38f2a8e1952a6da92c0d295a624d222db9f2
Matias Siro <matias.siro@outlook.com>
2021-11-30 19:51:29 +0200

    lisätty uusi testiluokka

--- project/src/test/java/project/CreateTablesTest.java
+++ project/src/test/java/project/CreateTablesTest.java
@@ -5,7 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class SQLReadingDAOTest {
+public class SQLCreateTablesTest {
 
     @Before
     public void setUp() {
